9ada2b4fbbdf23dafce4eac3d3ef1a08d920934a
acuaroo <milan.m.nair@gmail.com>
2024-04-10 11:58:59 -0600

    Added basic serialization syntax

--- plugin/Serialize.lua
+++ plugin/Serialize.lua
@@ -1,11 +1,40 @@
-local Serialize = {}
+local Serializer = {}
 
-function Serialize:EncodeModel()
+--[[
+  Serialization Syntax:
+  M: Model
+  P: Part
+  >: Child down
+  ^: Parent up
+  ,: Sibling
 
-end
+  [C;P;O;S] Properties
+    - C: XXXX: Color (BrickColor Index)
+    - P: x,y,z: Position (Vector3)
+    - O: x,y,z: Orientation (Vector3)
+    - S: x,y,z: Size (Vector3)
 
-function Serialize:DecodeModel()
+  Example:
+  M>P[...],P[...],P[...],M>P[...]^P[...]>P[...]>P[...]^^P[...]
+]]
+
+function Serializer:EncodeModel(base: Model | Part): string
   
 end
 
-return Serialize
+function Serializer:DecodeModel(data: string): nil
+  local baseModel = Instance.new("Model")
+  baseModel.Parent = workspace
+  baseModel.Name = "Generated Model"
+end
+
+function Serializer:_roundVector(vector: Vector3): Vector3
+  return Vector3.new(
+    math.floor(vector.X * 10) / 10,
+    math.floor(vector.Y * 10) / 10,
+    math.floor(vector.Z * 10) / 10
+  )
+end
+
+
+return Serializer
